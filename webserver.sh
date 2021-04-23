007b088b4f89d9dd38b06418c6b9f7d75f68c12d
vaishnavipund <vaishnavipund10@gmail.com>
2021-04-23 10:32:48 +0000

    Test for jenkins

--- webserver.sh
+++ webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "#########Starting the script#########"
 yum install -y httpd
-echo "<h1> THis webpage is created using Jenkins</h1> " > /var/www/html/index.html
+echo "<h1> THis webpage is created using Jenkins</h1>" > /var/www/html/index.html
 systemctl start httpd
 systemctl status httpd
 sleep 10
